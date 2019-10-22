ce51a0b46b89b9c64a99a51540a984a11771daa4
shin donggyu <donggyu9410@gmail.com>
2019-10-23 02:23:59 +0900

    asd

--- dist/WAS.js
+++ dist/WAS.js
@@ -5,5 +5,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var app_1 = __importDefault(require("./app"));
 require("./database/mongoose");
+var fs_1 = __importDefault(require("fs"));
+var http_1 = __importDefault(require("http"));
+var https_1 = __importDefault(require("https"));
+var privateKey = fs_1.default.readFileSync(__dirname + '/key/priv.pem');
+var certificate = fs_1.default.readFileSync(__dirname + '/key/cert.pem');
+var credentials = {
+    key: privateKey,
+    cert: certificate
+};
 var PORT = 4000;
-app_1.default.listen(PORT, function () { return console.log("KBU-MOBILE-STUDENT-ID-SERVER running on port " + PORT); });
+var httpServer = http_1.default.createServer(app_1.default);
+var httpsServer = https_1.default.createServer(credentials, app_1.default);
+var env = process.env.NODE_ENV ? process.env.NODE_ENV : 'env';
+if (env === 'env') {
+    httpServer.listen(PORT, function () { return console.log("KBU WAS http server listening on " + PORT); });
+}
+else {
+    httpsServer.listen(PORT, function () { return console.log("KBU WAS https server listening on " + PORT); });
+}
+// App.listen(PORT, () => console.log(`KBU-MOBILE-STUDENT-ID-SERVER running on port ${PORT}`))
--- src/WAS.ts
+++ src/WAS.ts
@@ -1,15 +1,30 @@
 import App from './app';
 import './database/mongoose'
-
-
+import fs from 'fs';
+import http from 'http';
+import https from 'https';
+const privateKey = fs.readFileSync(__dirname + '/key/priv.pem');
+const certificate = fs.readFileSync(__dirname + '/key/cert.pem');
+const credentials = {
+    key: privateKey,
+    cert: certificate
+}
 
 const PORT = 4000
 
+const httpServer = http.createServer(App);
+const httpsServer = https.createServer(credentials, App);
+
+let env = process.env.NODE_ENV ? process.env.NODE_ENV : 'env';
+
+if (env === 'env') {
+    httpServer.listen(PORT, () => console.log(`KBU WAS http server listening on ${PORT}`))
+} else {
+    httpsServer.listen(PORT, () => console.log(`KBU WAS https server listening on ${PORT}`));
+}
 
 
 
 
 
-
-
-App.listen(PORT, () => console.log(`KBU-MOBILE-STUDENT-ID-SERVER running on port ${PORT}`))
+// App.listen(PORT, () => console.log(`KBU-MOBILE-STUDENT-ID-SERVER running on port ${PORT}`))
